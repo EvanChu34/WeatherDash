bb6c7adfbe11fada65692239593b5a3293a447e5
Evan Chu <echu34@gmail.com>
2020-12-18 19:54:34 -0500

    changes to js and html

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 
 function startPage(){
-    var input = document.getElementById("#city-input")
-    var nameEl = document.getElementById("#selected-city")
-    var currentPic = document.getElementById("#current-pic")
-    var currentTemp = document.getElementById("#temperature");
-    var currentHumid = document.getElementById("#humidity");
-    var currentWindS = document.getElementById("#wind-speed");
-    var currentUVI = document.getElementById("#UV-index");
-    var cityHistory = document.getElementById("#city-list");
-    var cityList = JSON.parse(localStorage.getItem("#search")) || [];
+    const input = document.getElementById("city-input")
+    const nameEl = document.getElementById("selected-city")
+    const currentPic = document.getElementById("current-pic")
+    const currentTemp = document.getElementById("temperature");
+    const currentHumid = document.getElementById("humidity");
+    const currentWindS = document.getElementById("wind-speed");
+    const currentUVI = document.getElementById("UV-index");
+    const cityHistory = document.getElementById("city-list");
+    let cityList = JSON.parse(localStorage.getItem("search")) || [];
 
 
     var APIkey = "&appid=7deee8827dfbf279b7a9d2bd52377acb";
@@ -18,7 +18,7 @@ function startPage(){
     }
 
     function getWeatherInfo(cityName){
-        var queryURL = "https://api.openweathermap.org/data/2.5/weather?q="+ cityName + APIkey;
+        let queryURL = "https://api.openweathermap.org/data/2.5/weather?q="+ cityName + APIkey;
         $.ajax({
             url: queryURL,
             method: "GET"
@@ -32,14 +32,14 @@ function startPage(){
             nameEl.innerHTML = response.data.name + (month + "/" + day + "/" + year);
             var weatherIcon = response.data.weather[0].icon;
             currentPic.setAttribute("src","https://openweathermap.org/img/wn/" + weatherIcon + "@2x.png");
-            currentTemp.innerHTML = "Temperature: " + response.data.main.temp 
+            currentTemp.innerHTML = "Temperature: " + kelToFar(response.data.main.temp) 
             currentHumid.innerHTML = "Humidity: " + response.data.main.humidity + "%";
             currentWindS.innerHTML = "wind Speed: " + response.data.wind.speed + "MPH";       
         });
         // UV 
-        var lat = response.data.coord.lat;
-        var lon = response.data.coord.lon;
-        var UVQueryURL = "https://api.openweathermap.org/data/2.5/uvi/forecast?lat=" + lat + "&lon=" + lon + APIKey + "&cnt=1";
+        let lat = response.data.coord.lat;
+        let lon = response.data.coord.lon;
+        let UVQueryURL = "https://api.openweathermap.org/data/2.5/uvi/forecast?lat=" + lat + "&lon=" + lon + APIKey + "&cnt=1";
         $.ajax({
             url: UVQueryURL,
             method:"GET"
@@ -52,32 +52,32 @@ function startPage(){
             currentUVI.append(UVIndex);
         });
         // 5 day forcast 
-        var cityID = response.data.id;
-        var forecastQueryURL = "https://api.openweathermap.org/data/2.5/forecast?id=" + cityID + APIKey;
+        let cityID = response.data.id;
+        let forecastQueryURL = "https://api.openweathermap.org/data/2.5/forecast?id=" + cityID + APIKey;
         $.ajax({
             url: forecastQueryURL,
             method:"GET"
         }).then(function(response){
             console.log(response);
             const forcastCard = document.querySelectorAll(".forcast");
-            for (var i=0; i<forcastCard.length; i++){
+            for (i=0; i<forcastCard.length; i++){
                 forcastCard[i].innerHTML = "";
-                var cardIndex = i*8 + 4;
-                var cardDate = new Date(response.data.cityList[cardIndex].dt);
-                var cardDay = cardDate.getDate();
-                var cardMonth = cardDate.getMonth();
-                var cardYear = cardDate.getFullYear();
-                var cardForcastDate = document.createElement("p");
+                const cardIndex = i*8 + 4;
+                const cardDate = new Date(response.data.cityList[cardIndex].dt *1000);
+                const cardDay = cardDate.getDate();
+                const cardMonth = cardDate.getMonth();
+                const cardYear = cardDate.getFullYear();s
+                const cardForcastDate = document.createElement("p");
                 cardForcastDate.setAttribute("class","mt-3 mb-0 forcast-date");
                 cardForcastDate.innerHTML = cardMonth + "/" + cardDay + "/" + cardYear;
                 forcastCard[i].append(cardForcastDate);
-                var cardForcastWeather = document.createElement("img");
+                const cardForcastWeather = document.createElement("img");
                 cardForcastWeather.setAttribute("src","https://openweathermap.org/img/wn/" + response.data.list[cardIndex].weather[0].icon + "@2x.png");  
                 forcastCard[i].append(cardForcastWeather);
-                var cardTemp = document.createElement("p");
+                const cardTemp = document.createElement("p");
                 cardTemp.innerHTML = "Temp: " + response.data.list[cardIndex].main.temp;
                 forcastCard[i].append(cardTemp);
-                var cardHumid = document.createElement("p");
+                const cardHumid = document.createElement("p");
                 cardHumid.innerHTML = "Humidity: " + response.data.list[cardIndex].main.humidity + "%";
                 forcastCard[i].append(cardHumid);
             }
@@ -103,14 +103,16 @@ function startPage(){
             cityItem.setAttribute("class","form-contol d-block bg-white");
             cityItem.setAttribute("value", cityList[i]);
             cityItem.addEventListener("click",function(){
-                getWeatherInfo(cityItem.value);
+            getWeatherInfo(cityItem.value);
             })
             cityHistory.append(cityItem);
         }
-        createCityList();
-        if (cityList.length > 0){
-                getWeatherInfo(cityList[cityList.length - 1]);
-        }   
+
+    }   
+    
+    createCityList();
+    if (cityList.length > 0){
+        getWeatherInfo(cityList[cityList.length - 1]);
     }   
 
 }
