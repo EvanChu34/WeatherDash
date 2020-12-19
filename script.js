ef997062e6b7f10eeb93e7b1cbd7430c26be2628
Evan Chu <echu34@gmail.com>
2020-12-19 13:38:57 -0500

    Works kinda, icon too big and no cards yet

--- script.js
+++ script.js
@@ -22,21 +22,22 @@ function startPage(){
             method: "GET"
         }).then(function(response){
             console.log(response);
-            var currentDate = new Date(response.data.dt*1000);
+            var currentDate = new Date(response.dt*1000);
             console.log(currentDate);
             var day = currentDate.getDate();
             var month = currentDate.getMonth() + 1;
             var year = currentDate.getFullYear();
-            nameEl.innerHTML = response.data.name + (month + "/" + day + "/" + year);
-            var weatherIcon = response.data.weather[0].icon;
-            currentPic.setAttribute("src","https://openweathermap.org/img/wn/" + weatherIcon + "@2x.png");
-            currentTemp.innerHTML = "Temperature: " + kelToFar(response.data.main.temp) 
-            currentHumid.innerHTML = "Humidity: " + response.data.main.humidity + "%";
-            currentWindS.innerHTML = "wind Speed: " + response.data.wind.speed + "MPH"; 
+            nameEl.innerHTML = response.name + " (" + month + "/" + day + "/" + year +") " ;
+            let weatherIcon = response.weather[0].icon;
+            currentPic.setAttribute("src","https://openweathermap.org/img/wn/"+weatherIcon+"@2x.png");
+            currentPic.setAttribute("alt",response.weather[0].description);
+            currentTemp.innerHTML = "Temperature: " + kelToFar(response.main.temp) + " &#176F"; 
+            currentHumid.innerHTML = "Humidity: " + response.main.humidity + "%";
+            currentWindS.innerHTML = "Wind Speed: " + response.wind.speed + "MPH"; 
         // UV 
-        let lat = response.data.coord.lat;
-        let lon = response.data.coord.lon;
-        let UVQueryURL = "https://api.openweathermap.org/data/2.5/uvi/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + APIKey + "&cnt=1";
+        let lat = response.coord.lat;
+        let lon = response.coord.lon;
+        let UVQueryURL = "https://api.openweathermap.org/data/2.5/uvi/forecast?lat=" + lat + "&lon=" + lon + "&appid=7deee8827dfbf279b7a9d2bd52377ac&cnt=1";
         $.ajax({
             url: UVQueryURL,
             method:"GET"
@@ -48,8 +49,8 @@ function startPage(){
             currentUVI.append(UVIndex);
         });
         // 5 day forcast 
-        let cityID = response.data.id;
-        let forecastQueryURL = "https://api.openweathermap.org/data/2.5/forecast?id=" + cityID +"&appid=" + APIKey;
+        let cityID = response.id;
+        let forecastQueryURL = "https://api.openweathermap.org/data/2.5/forecast?id=" + cityID +"&appid=7deee8827dfbf279b7a9d2bd52377ac";
         $.ajax({
             url: forecastQueryURL,
             method:"GET"
@@ -71,10 +72,10 @@ function startPage(){
                 cardForcastWeather.setAttribute("src","https://openweathermap.org/img/wn/" + response.data.list[cardIndex].weather[0].icon + "@2x.png");  
                 forcastCard[i].append(cardForcastWeather);
                 const cardTemp = document.createElement("p");
-                cardTemp.innerHTML = "Temp: " + response.data.list[cardIndex].main.temp;
+                cardTemp.innerHTML = "Temp: " + response.list[cardIndex].main.temp;
                 forcastCard[i].append(cardTemp);
                 const cardHumid = document.createElement("p");
-                cardHumid.innerHTML = "Humidity: " + response.data.list[cardIndex].main.humidity + "%";
+                cardHumid.innerHTML = "Humidity: " + response.list[cardIndex].main.humidity + "%";
                 forcastCard[i].append(cardHumid);
             }
         })});
@@ -82,7 +83,7 @@ function startPage(){
 
     // search button
     searchBtn.addEventListener("click", function(){
-        const searchCity = input.Value;
+        const searchCity = input.value;
         getWeatherInfo(searchCity);
         cityList.push(searchCity);
         localStorage.setItem("search",JSON.stringify(cityList));
